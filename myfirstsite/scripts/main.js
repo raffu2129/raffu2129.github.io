7473d387b5f372e416294e98184afd84dac0cb02
raffu2129 <54778742+raffu2129@users.noreply.github.com>
2019-09-02 12:05:25 +0200

    Add files via upload

---
+++ myfirstsite/scripts/main.js
@@ -0,0 +1,29 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/landscape1.jpg') {
+      myImage.setAttribute ('src','images/landscape2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/landscape1.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome .., ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome Back ..!!!, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
